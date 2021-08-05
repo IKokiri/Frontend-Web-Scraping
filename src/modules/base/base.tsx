124dd0293eba8b6532235c73264606a86af69610
Luiz Mendes <luiz.mendes@in8.com.br>
2021-08-05 10:06:50 -0300

    refactor: remove useless icon

--- src/modules/base/base.tsx
+++ src/modules/base/base.tsx
@@ -20,7 +20,6 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import ExitIcon from '@material-ui/icons/PowerSettingsNew';
 
 const drawerWidth = 240;
