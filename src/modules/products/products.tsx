40facc2ac3c10d70d44b8624172e0d91becda019
Luiz Mendes <luiz.mendes@in8.com.br>
2021-08-05 21:20:40 -0300

    feat: add product component

---
+++ src/modules/products/products.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CardProduct from './components/cardProduct/CardProduct';
+
+function Products(): JSX.Element {
+  return <CardProduct />;
+}
+
+export default Products;
