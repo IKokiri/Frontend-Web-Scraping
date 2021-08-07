d362240726c9ecd2db7239169e150a92b02b81da
Luiz Mendes <luiz.mendes@in8.com.br>
2021-08-07 12:50:11 -0300

    fix: add id in edit rout product

--- src/modules/base/base.tsx
+++ src/modules/base/base.tsx
@@ -175,7 +175,7 @@ export default function PersistentDrawerLeft() {
               <Route exact path="/admin/product">
                 <Products />
               </Route>
-              <Route exact path="/admin/product/edit/:id">
+              <Route exact path="/admin/product/edit/">
                 <EditProduct />
               </Route>
             </Switch>
